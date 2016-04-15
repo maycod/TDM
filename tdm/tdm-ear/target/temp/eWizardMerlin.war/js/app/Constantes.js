6981742493d76b6beecf8086c419fec963ed279c
Manuel C. Maycotte <mcm_maycod@hotmail.com>
2016-04-14 20:17:28 -0500

    Target Folders Removed

--- tdm/tdm-ear/target/temp/eWizardMerlin.war/js/app/Constantes.js
+++
@@ -1,1 +0,0 @@
-var contexto = "/merlin-wizard-webapp/rest";
--- tdm/tdm-ear/target/temp/eWizardMerlin.war/js/app/view/ComboTiposPedidoView.js
+++
@@ -1,8 +0,0 @@
-merlinwizard.ComboTiposPedidoView = Backbone.View.extend({
-tagName: 'option',
-render: function(){
-	 jQuery(this.el).attr('value',     this.model.get('id')).html(this.model.get('descripcion'));
-		      
-  return this; 
-}
-});
